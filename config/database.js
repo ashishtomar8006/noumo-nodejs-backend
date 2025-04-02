0882c98b4317e5d62c830910b1a16149f25b1842
ashishtomar8006 <ashishtomar.liangtuang@gmail.com>
2025-04-02 19:03:33 +0530

    commit

--- config/database.js
+++ config/database.js
@@ -4,10 +4,16 @@ import dotenv from "dotenv"
 // Load environment variables
 dotenv.config()
 
-const sequelize = new Sequelize('noumo_db', "root", "", {
-  host: "127.0.0.1",
+// const sequelize = new Sequelize('noumo_db', "root", "", {
+//   host: "127.0.0.1",
+//   dialect: "mysql",
+//   port: process.env.DB_PORT || 3306,
+// });
+
+const sequelize = new Sequelize("bcfhcbl92qlh0fqorpgs", "urd1j91nu1qnslnx", "smCLzUKYRWheAVmXaCMR", {
+  host: "bcfhcbl92qlh0fqorpgs-mysql.services.clever-cloud.com",
   dialect: "mysql",
-  port: process.env.DB_PORT || 3306,
+  port: 3306,
 });
 
 const connectDB = async () => {
